d5e8fe6d2f56bbcaf07ba219fe8af24feca9fbb8
KingaMendyk <s25275@pjwstk.edu.pl>
2024-01-19 12:04:34 +0100

    Funkcja do testowania
    Funkcja do testowania tasowania w GameManagerze

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
@@ -12,5 +13,27 @@ public class GameManager : MonoBehaviour {
         if (drawnCard != null) {
             handManager.AddCardToHand(drawnCard);
         }
+    }
+
+    // Call this method to test shuffling the deck
+    public void TestShuffle()
+    {
+        string ids = "Before shuffle: [";
+        foreach (BaseCardData card in deckManager.deck)
+        {
+            ids += card.id + ", ";
+        }
+        ids += "]";
+        Debug.Log(ids);
+        
+        deckManager.Shuffle();
+        
+        ids = "After shuffle: [";
+        foreach (BaseCardData card in deckManager.deck)
+        {
+            ids += card.id + ", ";
+        }
+        ids += "]";
+        Debug.Log(ids);
     }
 }
