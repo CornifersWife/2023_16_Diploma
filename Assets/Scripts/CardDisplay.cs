b29c590571aabfcb378122af31c2546606192d98
KingaMendyk <s25275@pjwstk.edu.pl>
2024-01-21 18:08:51 +0100

    Śledzenie karty
    UI karty przyczepione jest do karty i przy przesuwaniu karty nie zostaje już w miejscu

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -8,7 +8,7 @@ public class CardDisplay : MonoBehaviour {
     public BaseCardData cardData;
     public MinionCardData minionCardData;
     public GameObject cardCanvas;
-
+    
     public void SetupCard(BaseCardData data) {
         cardData = data;
         // Update the card's visuals here
@@ -22,14 +22,23 @@ public class CardDisplay : MonoBehaviour {
     public void DisplayData(GameObject card)
     {
         GameObject cardCanvas = Instantiate(this.cardCanvas, card.transform.position, transform.rotation * Quaternion.Euler (90f, 0f, 180f));
+        cardCanvas.transform.SetParent(card.transform);
         GameObject cardText = cardCanvas.transform.GetChild(0).gameObject;
 
-        GameObject manaText = cardText.transform.GetChild(0).gameObject;
-        GameObject attackText = cardText.transform.GetChild(1).gameObject;
-        GameObject healthText = cardText.transform.GetChild(2).gameObject;
+        GameObject idText = cardText.transform.GetChild(0).gameObject;
+        GameObject nameText = cardText.transform.GetChild(1).gameObject;
+        GameObject manaText = cardText.transform.GetChild(2).gameObject;
+        GameObject attackText = cardText.transform.GetChild(3).gameObject;
+        GameObject healthText = cardText.transform.GetChild(4).gameObject;
         
+        idText.GetComponent<TextMeshProUGUI>().text = cardData.id.ToString();
+        nameText.GetComponent<TextMeshProUGUI>().text = cardData.cardName;
         manaText.GetComponent<TextMeshProUGUI>().text = cardData.cost.ToString();
-        attackText.GetComponent<TextMeshProUGUI>().text = cardData.cardName.ToString();
-        healthText.GetComponent<TextMeshProUGUI>().text = cardData.id.ToString();
+        
+        //If card is Minion:
+        /*
+        attackText.GetComponent<TextMeshProUGUI>().text = cardData.power.ToString();
+        healthText.GetComponent<TextMeshProUGUI>().text = cardData.health.ToString();
+        */
     }
 }
