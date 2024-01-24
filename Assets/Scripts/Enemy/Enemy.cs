7d08d5d5ce1de70b678d40efd30b1bf4c4edafdc
KingaMendyk <s25275@pjwstk.edu.pl>
2024-01-24 14:08:04 +0100

    Update scene
    Update sceny i skryptu, żeby działały z nowym systemem kart

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -14,15 +14,7 @@ public class Enemy : MonoBehaviour
 
     public void Start()
     {
-        //move this to card position manager if possible
-        //------------------------------------------------
-        positionAvailabilityMap = new Dictionary<Vector3, bool>();
-
-        for (int i = 0; i < cardPositionManager._opponentPositions.Count; i++)
-        {
-            positionAvailabilityMap.Add(cardPositionManager._opponentPositions[i], true);
-        }
-        //------------------------------------------------
+        
     }
 
     public void PlayCard()
@@ -32,32 +24,10 @@ public class Enemy : MonoBehaviour
         {
             //take random index
             int index = random.Next(0, enemyHandManager.hand.Count);
-            CardDisplay playedCard = enemyHandManager.hand[index];
-            enemyHandManager.hand.Remove(playedCard);
+            BaseCardData playedCard = enemyHandManager.PlayCard(index);
             
-            List<Vector3> aSpots = AvailableSpots();
-            //check available spots
-            if (aSpots.Count != 0)
-            {
-                //take random position on board
-                Vector3 pos = aSpots[random.Next(0, aSpots.Count())];
-                playedCard.transform.position = pos;
-
-                positionAvailabilityMap[pos] = false;
-            }
+            index = random.Next(0, boardSideManager.count);
+            boardSideManager.AddCardToBoard(playedCard, index);
         }
-    }
-
-    private List<Vector3> AvailableSpots()
-    {
-        List<Vector3> res = new List<Vector3>();
-        foreach (Vector3 v in positionAvailabilityMap.Keys)
-        {
-            if (positionAvailabilityMap[v])
-            {
-                res.Add(v);
-            }
-        }
-        return res;
     }
 }
