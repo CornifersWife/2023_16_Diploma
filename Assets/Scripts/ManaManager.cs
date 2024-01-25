dd27a02214c0bab750a69b3d0f6479fc7bc9d7dd
AleksandraDemidziuk <s25241@pjwstk.edu.pl>
2024-01-25 14:14:00 +0100

    ManaMenager_v2_+_HeroHealth_v1

--- Assets/Scripts/ManaManager.cs
+++ Assets/Scripts/ManaManager.cs
@@ -1,22 +1,29 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManaManager : MonoBehaviour {
     private int actualMana;
     private int usedMana;
+    public TextMeshPro manaCount;
 
     //Use at the beginning of the game
     public void GameStart() {
+        manaCount = GetComponent<TextMeshPro>();
         actualMana = 1;
+        manaCount.text = "Mana: " + actualMana;
     }
 
     //Use this method to buy a card
-    public void UseMana(Card card) {
+    public bool UseMana(Card card) {
         if (actualMana >= card.cost) {
             actualMana -= card.cost;
             usedMana += card.cost;
+            manaCount.text = "Mana: " + actualMana;
+            return true;
         }else {
-            throw new NotImplementedException(); 
+            return false;
         }
     }
 
@@ -26,5 +33,6 @@ public class ManaManager : MonoBehaviour {
         if (actualMana < 10) {
             actualMana++;
         }
+        manaCount.text = "Mana: " + actualMana;
     }
 }
