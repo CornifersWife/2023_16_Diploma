7be94f3b25ea2f2fb6b1f12273adec933000046f
KingaMendyk <s25275@pjwstk.edu.pl>
2024-03-11 22:31:03 +0100

    Update HoverName.cs

--- Assets/HoverName.cs
+++ Assets/HoverName.cs
@@ -8,36 +8,17 @@ public class Name : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private float timeToWait = 0.5f;
     [SerializeField] private string messageToShow;
-    [SerializeField] private GameObject player;
-
-    private bool _playerIsNear = false;
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (_playerIsNear)
-        {
-            StopAllCoroutines();
-            StartCoroutine(MessageShowTimer());
-        }
+        StopAllCoroutines();
+        StartCoroutine(MessageShowTimer());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         StopAllCoroutines();
         ShowNameManager.OnMouseLoseFocus();
-    }
-    
-    void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
-            Debug.Log("Player nearby");
-            _playerIsNear = true;
-        }
-    }
-	
-    void OnTriggerExit(Collider other) {
-        if (other.CompareTag("Player")) {
-            Debug.Log("Player far away");
-            _playerIsNear = false;
-        }
     }
 
     private void ShowMessage()
