f477d935baffe5b9273a5e368457b34606523953
CornifersWife <irys.latosz@gmail.com>
2024-03-12 16:46:42 +0100

    Attack Animation based on distance

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -9,8 +9,9 @@ public class CardDisplay : MonoBehaviour {
     public delegate void OnDestroyedDelegate();
     public event OnDestroyedDelegate OnDestroyed;
 
-    private bool inAnimation=false;
-    
+    private bool inAnimation;
+
+    [SerializeField] private float distanceImportance = 1f;
     [SerializeField] private float attackAnimationDuration = 0.2f; // Duration of the entire attack movement
     [SerializeField] private float easeOutDurationFraction = 0.6f; // Fraction of the duration for easing out
     [SerializeField] private float easeInDurationFraction = 0.4f; // Fraction of the duration for easing in
@@ -18,42 +19,44 @@ public class CardDisplay : MonoBehaviour {
 
     [SerializeField] private float deathAnimationRotationDuration = 0.2f;
     [SerializeField] private float deathAnimationStopDuration = 0.1f;
+
     public void SetupCard(BaseCardData data) {
         cardData = data;
         DisplayData(gameObject);
     }
-    
+
     public void SetupCard(MinionCardData minionData) {
         cardData = minionData;
         minionData.currentHealth = minionData.maxHealth;
-        minionData.OnHealthChanged += UpdateHealthDisplay; 
+        minionData.OnHealthChanged += UpdateHealthDisplay;
         minionData.OnAttack += AttackTarget;
         minionData.OnRequestPosition += GetCardPosition;
         minionData.OnDeath += Destroy;
         DisplayData(gameObject);
     }
 
-    Vector3 GetCardPosition()
-    {
+    Vector3 GetCardPosition() {
         return transform.position;
     }
+
     public void AttackTarget(IDamageable target) {
         StartCoroutine(MoveTowardsTarget(target.GetPosition()));
     }
-    
+
     IEnumerator MoveTowardsTarget(Vector3 targetPosition) {
         inAnimation = true;
         targetPosition.y += 0.1f; //avoids mesh fighting
         //TODO animationTimeBased On Distance to target
         float elapsedTime = 0.0f;
         Vector3 startPosition = transform.position;
+        float distanceToTarget = Vector3.Distance(this.transform.position, targetPosition);
+        float distanceFactor = distanceImportance * (float)Math.Sqrt(distanceToTarget);
+        attackAnimationDuration *= distanceFactor;
+        float easeOutDuration = easeOutDurationFraction * attackAnimationDuration ;
+        float easeInDuration = easeInDurationFraction * attackAnimationDuration ;
 
-        float easeOutDuration = attackAnimationDuration * easeOutDurationFraction;
-        float easeInDuration = attackAnimationDuration * easeInDurationFraction;
-
-        // Ease out (move towards the target)
-        while (elapsedTime < easeOutDuration)
-        {
+        // Ease out 
+        while (elapsedTime < easeOutDuration) {
             elapsedTime += Time.deltaTime;
             float t = elapsedTime / easeOutDuration;
             transform.position = Vector3.Lerp(startPosition, targetPosition, t);
@@ -62,10 +65,9 @@ public class CardDisplay : MonoBehaviour {
 
         yield return new WaitForSeconds(impactPauseDuration);
 
-        // Ease in (return to start position)
-        elapsedTime = 0.0f; 
-        while (elapsedTime < easeInDuration)
-        {
+        // Ease in 
+        elapsedTime = 0.0f;
+        while (elapsedTime < easeInDuration) {
             elapsedTime += Time.deltaTime;
             float t = elapsedTime / easeInDuration;
             transform.position = Vector3.Lerp(targetPosition, startPosition, t);
@@ -75,13 +77,14 @@ public class CardDisplay : MonoBehaviour {
         transform.position = startPosition;
         inAnimation = false;
     }
-    private void OnDestroy()
-    {
+
+    private void OnDestroy() {
         OnDestroyed?.Invoke();
     }
+
     private void Destroy(MinionCardData minionData) {
         StartCoroutine(DeathSequence());
-        minionData.OnHealthChanged -= UpdateHealthDisplay; 
+        minionData.OnHealthChanged -= UpdateHealthDisplay;
         minionData.OnAttack -= AttackTarget;
         minionData.OnRequestPosition -= GetCardPosition;
         minionData.OnDeath -= Destroy;
@@ -92,30 +95,29 @@ public class CardDisplay : MonoBehaviour {
         yield return StartCoroutine(DeathAnimation());
         Destroy(gameObject);
     }
-    private IEnumerator DeathAnimation()
-    {
-        float time = 0; 
-        Quaternion startRotation = transform.rotation; 
-        Quaternion endRotation = transform.rotation * Quaternion.Euler(0, 30, 0); 
 
-        while (time < deathAnimationRotationDuration)
-        {
+    private IEnumerator DeathAnimation() {
+        float time = 0;
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = transform.rotation * Quaternion.Euler(0, 30, 0);
+
+        while (time < deathAnimationRotationDuration) {
             time += Time.deltaTime;
             float t = time / deathAnimationRotationDuration;
             transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
             yield return null;
         }
+
         transform.rotation = endRotation;
         yield return new WaitForSeconds(deathAnimationStopDuration);
     }
+
     private void Update() {
-        if (cardData is MinionCardData)
-        {
+        if (cardData is MinionCardData) {
             MinionCardData minionCardData = (MinionCardData)cardData;
             string newHealthText = minionCardData.currentHealth.ToString();
 
-            if (_healthText != newHealthText)
-            {
+            if (_healthText != newHealthText) {
                 _healthText = newHealthText;
                 UpdateHealthDisplay(minionCardData.currentHealth);
             }
@@ -126,40 +128,39 @@ public class CardDisplay : MonoBehaviour {
         string healthText = newHealth.ToString();
         GameObject canvas = gameObject.transform.GetChild(0).gameObject;
         GameObject cardText = canvas.transform.GetChild(0).gameObject;
-        GameObject healthTextGameObject = cardText.transform.GetChild(3).gameObject; // Assuming health text is at child index 3
+        GameObject
+            healthTextGameObject =
+                cardText.transform.GetChild(3).gameObject; // Assuming health text is at child index 3
         healthTextGameObject.GetComponent<TextMeshProUGUI>().text = healthText;
         _healthText = healthText; // Update the cached health text
     }
 
 
-    public void DisplayData(GameObject card)
-    {
+    public void DisplayData(GameObject card) {
         GameObject canvas = card.transform.GetChild(0).gameObject;
         GameObject cardText = canvas.transform.GetChild(0).gameObject;
-        
+
         GameObject nameText = cardText.transform.GetChild(0).gameObject;
         GameObject manaText = cardText.transform.GetChild(1).gameObject;
         GameObject attackText = cardText.transform.GetChild(2).gameObject;
         GameObject healthText = cardText.transform.GetChild(3).gameObject;
-        
+
         //GameObject Image = canvas.transform.GetChild(1).gameObject;
-        
+
         nameText.GetComponent<TextMeshProUGUI>().text = cardData.cardName;
         manaText.GetComponent<TextMeshProUGUI>().text = cardData.cost.ToString();
         //Image.GetComponent<Image>().sprite = cardData.cardImage;
-        
+
         //If card is Minion:
         MinionCardData minionCardData = (MinionCardData)cardData;
-        if (cardData is MinionCardData)
-        {
+        if (cardData is MinionCardData) {
             attackText.GetComponent<TextMeshProUGUI>().text = minionCardData.power.ToString();
             healthText.GetComponent<TextMeshProUGUI>().text = minionCardData.currentHealth.ToString();
             _healthText = healthText.GetComponent<TextMeshProUGUI>().text;
         }
 
         //If card is not Minion:
-        else
-        {
+        else {
             attackText.GetComponent<TextMeshProUGUI>().text = "";
             healthText.GetComponent<TextMeshProUGUI>().text = "";
         }
