e51e23cb1e3c26bff184c53caafddf13c629739d
KingaMendyk <s25275@pjwstk.edu.pl>
2024-03-17 16:56:33 +0100

    NPC random walking
    Added Navigation Agent to NPC

---
+++ Assets/NPCRandomWalk.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCRandomWalk : MonoBehaviour
+{
+    [Space(10)]
+    [Header("Ranges")]
+    [Range(-100, 100)]
+    [SerializeField] private int xLocalRange;
+    [Range(-100, 100)]
+    [SerializeField] private int xGlobalRange;
+    [Range(-100, 100)]
+    [SerializeField] private int zLocalRange;
+    [Range(-100, 100)]
+    [SerializeField] private int zGlobalRange;
+    
+    private NavMeshAgent navMeshAgent;
+    private bool waypointSet = false;
+    private Vector3 destination;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Walk();
+    }
+
+    private void Walk()
+    {
+        if (waypointSet)
+        {
+            navMeshAgent.SetDestination(destination);
+        }
+
+        if (Vector3.Distance(transform.position, destination) < 0.1f)
+        {
+            waypointSet = false;
+        }
+
+        if (!waypointSet)
+        {
+            SearchForWaypoint();
+        }
+    }
+
+    private void SearchForWaypoint()
+    {
+        float x = Random.Range(-xLocalRange, xLocalRange);
+        float z = Random.Range(-zLocalRange, zLocalRange);
+
+        destination = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+        destination.x = Validate(destination.x, xGlobalRange);
+        destination.z = Validate(destination.z, zGlobalRange);
+        
+        waypointSet = true;
+    }
+
+    private float Validate(float coord, float range)
+    {
+        if (coord > range)
+            return range;
+        if (coord < -range)
+            return -range;
+        return coord;
+    }
+}
