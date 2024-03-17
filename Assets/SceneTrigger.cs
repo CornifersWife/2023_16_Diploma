08bc77a3745265bfcba76ed83c4bdf3b131e960c
KingaMendyk <s25275@pjwstk.edu.pl>
2024-03-17 16:06:04 +0100

    Update SceneTrigger.cs
    Fixed class name

--- Assets/SceneTrigger.cs
+++ Assets/SceneTrigger.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SceneTrigger : MonoBehaviour
 {
     [SerializeField] private string loadName;
     [SerializeField] private string unloadName;
