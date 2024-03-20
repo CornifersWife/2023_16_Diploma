0ac038d6f673d3b656af6e7f8fe127dd22e48dce
KingaMendyk <s25275@pjwstk.edu.pl>
2024-03-20 10:44:15 +0100

    Create Scene trigger popup
    Switch scene when Yes clicked on popup

---
+++ Assets/SceneTriggerPopup.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SceneTriggerPopup : MonoBehaviour
+{
+    [SerializeField] private string loadName;
+    [SerializeField] private string unloadName;
+    [SerializeField] private RectTransform popupPanel;
+    public GameObject player;
+
+    private void Awake()
+    {
+        popupPanel.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<PlayerController>().enabled = false;
+        popupPanel.gameObject.SetActive(true);
+    }
+
+    public void YesClicked()
+    {
+        if (loadName != "")
+            SceneSwitcher.Instance.LoadScene(loadName);
+        if(unloadName != "")
+            SceneSwitcher.Instance.UnloadScene(unloadName);
+    }
+
+    public void NoClicked()
+    {
+        popupPanel.gameObject.SetActive(false);
+        player.GetComponent<PlayerController>().enabled = true;
+    }
+}
