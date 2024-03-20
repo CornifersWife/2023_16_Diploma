afae81ee79359be5f9e26a5f15f5eb9021677154
CornifersWife <irys.latosz@gmail.com>
2024-03-20 10:52:49 +0100

    Update Hero.cs

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,10 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class Hero : MonoBehaviour, IDamageable
 {
     public int maxHealth = 20;
     public int currentHealth;
-
+    public float moveDistance = 0.5f;
+    public float animationDuration = 0.1f;
     private void Awake()
     {
         currentHealth = maxHealth;
@@ -13,12 +15,33 @@ public class Hero : MonoBehaviour, IDamageable
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        StartCoroutine(AnimateTakingDamage());
         if (currentHealth <= 0)
         {
             Death();
         }
+        
     }
 
+    public IEnumerator AnimateTakingDamage() {
+        Vector3 originalPosition = transform.position;
+        Vector3 targetPosition = originalPosition - new Vector3(0, 0, moveDistance); 
+
+        float elapsedTime = 0;
+        while (elapsedTime < animationDuration / 2) {
+            transform.position = Vector3.Lerp(originalPosition, targetPosition, (elapsedTime / (animationDuration / 2)));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        elapsedTime = 0;
+
+        while (elapsedTime < animationDuration / 2) {
+            transform.position = Vector3.Lerp(targetPosition, originalPosition, (elapsedTime / (animationDuration / 2)));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = originalPosition;
+    }
     public int GetPower() {
         return 0;
     }
