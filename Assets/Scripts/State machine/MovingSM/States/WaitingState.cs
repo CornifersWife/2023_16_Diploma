e5a3e6558454b2dc235863a41958cba6b82f8ced
KingaMendyk <s25275@pjwstk.edu.pl>
2024-03-30 19:16:17 +0100

    Added states for npc walking

---
+++ Assets/Scripts/State machine/MovingSM/States/WaitingState.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class WaitingState : BaseState {
+    private MovingSM movingSM;
+    private float waitCounter;
+    private float waitTime;
+
+    public WaitingState(MovingSM stateMachine) : base("Waiting", stateMachine) {
+        movingSM = stateMachine;
+    }
+
+    public override void Enter() {
+        waitTime = movingSM.GetWaitTime();
+    }
+
+    public override void UpdateLogic() {
+        base.UpdateLogic();
+        if (movingSM.IsWaiting())
+            Wait();
+        else{
+            movingSM.ChangeState(movingSM.walkingState);
+        }
+    }
+
+    public override void Exit() {
+        base.Exit();
+        waitCounter = 0f;
+    }
+
+    private void Wait() {
+        waitCounter += Time.deltaTime;
+        if (waitCounter >= waitTime)
+            movingSM.SetWaiting(false);
+    }
+}    
---
+++ Assets/Scripts/State machine/MovingSM/States/WalkingState.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkingState : BaseState {
+    private MovingSM movingSM;
+    private NavMeshAgent navMeshAgent;
+    private Transform[] waypoints;
+    private int currentWaypointIndex;
+    private Vector3 currentWaypoint;
+    public WalkingState(MovingSM stateMachine) : base("Walking", stateMachine) {
+        movingSM = stateMachine;
+    }
+
+    public override void Enter() {
+        base.Enter();
+        navMeshAgent = movingSM.GetNavMeshAgent();
+        waypoints = movingSM.GetWaypoints();
+        currentWaypointIndex = movingSM.GetCurrentWaypointIndex();
+    }
+
+    public override void UpdateLogic() {
+        base.UpdateLogic();
+        UpdateWaypoint();
+        if (Vector3.Distance(navMeshAgent.transform.position, currentWaypoint) < Mathf.Epsilon) {
+            SetWaypoint();
+            movingSM.SetWaiting(true);
+            movingSM.ChangeState(movingSM.waitingState);
+        }
+        else {
+            Move();
+        }
+    }
+    
+    private void Move() {
+        navMeshAgent.SetDestination(currentWaypoint);
+    }
+    
+    private void SetWaypoint() {
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        movingSM.SetCurrentWaypointIndex(currentWaypointIndex);
+    }
+
+    private void UpdateWaypoint() {
+        currentWaypoint = waypoints[currentWaypointIndex].position;
+        currentWaypoint.y = navMeshAgent.transform.position.y;
+    }
+}
