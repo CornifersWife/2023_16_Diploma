83dc8b57af28a082399510126a3a77fc5b761be8
KingaMendyk <s25275@pjwstk.edu.pl>
2024-04-13 13:13:02 +0200

    FIX player moves to (0,0,0) on game start

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
@@ -19,6 +18,7 @@ public class PlayerController : MonoBehaviour, IWalkable {
         mainCamera = Camera.main;
         navMeshAgent = GetComponent<NavMeshAgent>();
         groundLayer = LayerMask.NameToLayer("Ground");
+        targetPosition = transform.position;
         UIGameObject = GameObject.Find("UI");
         activeUIOnStart = GetActiveUIOnStart();
     }
