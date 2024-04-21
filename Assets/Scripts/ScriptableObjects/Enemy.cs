3e3be9082893291d5f427a13f383e82f83a5bec4
KingaMendyk <s25275@pjwstk.edu.pl>
2024-04-21 15:56:42 +0200

    Enemy deck is loaded
    Added deck to Enemy Scriptable Object
    Added a bool variable for ticking if the player or enemy should be loaded. Could be changed in the future

--- Assets/Scripts/ScriptableObjects/Enemy.cs
+++ Assets/Scripts/ScriptableObjects/Enemy.cs
@@ -1,11 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Enemy", menuName = "Enemy")]
 public class Enemy : ScriptableObject {
     [SerializeField] private string enemyName;
+    [SerializeField] private List<CardSetData> deck = new List<CardSetData>();
     public EnemyState state;
 
     public void ChangeState(EnemyState enemyState) {
         state = enemyState;
+    }
+
+    public List<CardSetData> GetDeck()
+    {
+        return deck;
     }
 }
