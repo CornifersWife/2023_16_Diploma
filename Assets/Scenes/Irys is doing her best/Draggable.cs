aee2c98bf33917ad4eb77868719319d88466aa0c
CornifersWife <irys.latosz@gmail.com>
2024-05-11 17:33:51 +0200

    initial commit

---
+++ Assets/Scenes/Irys is doing her best/Draggable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+
+InputSystemUIInputModule
+public class Draggable : MonoBehaviour, ISelectable;
+{
+    
+}
