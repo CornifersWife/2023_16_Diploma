8216b2a3d0ea7024416d7eacd9630592b6854df4
CornifersWife <irys.latosz@gmail.com>
2024-05-21 14:44:54 +0200

    added a new scece

---
+++ Assets/Scenes/Irys is doing her best/CardSpot2.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+    public class CardSpot2 : IDropTarget{
+        
+    }
--- Assets/Scenes/Irys is doing her best/Draggable.cs
+++ Assets/Scenes/Irys is doing her best/Draggable.cs
@@ -1,12 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem.UI;
 
-InputSystemUIInputModule
-public class Draggable : MonoBehaviour, ISelectable;
-{
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+    private Vector3 offset;
+    private Camera mainCamera;
+    private Transform originalTransform;
+
+    private void Start() {
+        mainCamera = Camera.main;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        var startingPosition = transform.position;
+        this.transform.position = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+    }
     
+    public void OnBeginDrag(PointerEventData eventData) {
+        originalTransform = transform;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        throw new System.NotImplementedException();
+    }
 }
