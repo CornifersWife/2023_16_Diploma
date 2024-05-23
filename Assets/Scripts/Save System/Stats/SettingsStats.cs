a1681fcc7f830b9aa929e0e79e48e54542ddd259
KingaMendyk <s25275@pjwstk.edu.pl>
2024-05-23 19:30:27 +0200

    Added stats classes

---
+++ Assets/Scripts/Save System/Stats/SettingsStats.cs
@@ -0,0 +1,10 @@
+public class SettingsStats {
+    public float musicVolume;
+    public float sfxVolume;
+    public int resolutionIndex;
+    public bool isFullscreen;
+
+    public SettingsStats() {
+        
+    }
+}
