4f5e8663973406599284240dc59a399e80865e12
CornifersWife <irys.latosz@gmail.com>
2024-06-25 19:23:23 +0200

    added framework to add animations to effects

--- Assets/Scripts/CardBattles/Managers/EffectManager.cs
+++ Assets/Scripts/CardBattles/Managers/EffectManager.cs
@@ -1,38 +1,45 @@
+using System.Collections;
 using System.Collections.Generic;
 using CardBattles.Enums;
 using CardBattles.Interfaces;
 using UnityEngine;
-using Component = System.ComponentModel.Component;
 
 namespace CardBattles.Managers {
     public static class EffectManager {
-        
-        
-        public delegate void EffectDelegate(List<GameObject> targets, int value);
+        private static Dictionary<EffectName, EffectAnimationManager.EffectAnimationDelegate> Animation =>
+            EffectAnimationManager.Animation;
 
-        public static Dictionary<EffectName, EffectDelegate> 
+        public delegate IEnumerator EffectDelegate(List<GameObject> targets, int value);
+
+        public static Dictionary<EffectName, EffectDelegate>
             effectDictionary =
-            new Dictionary<EffectName, EffectDelegate> {
-                { EffectName.Heal , Heal },
-                { EffectName.DealDamage, DealDamage },
-                { EffectName.ChangeAttack, ChangeAttack}
-            };
+                new() {
+                    { EffectName.Heal, Heal },
+                    { EffectName.DealDamage, DealDamage },
+                    { EffectName.ChangeAttack, ChangeAttack }
+                };
 
-        private static void Heal(List<GameObject> targets, int heal) {
+
+        private static IEnumerator Heal(List<GameObject> targets, int heal) {
             foreach (var target in targets) {
-                if(target.TryGetComponent(typeof(IDamageable), out var component))
+                if (target.TryGetComponent(typeof(IDamageable), out var component)) {
                     ((IDamageable)component).Heal(heal);
+                    yield return Animation[EffectName.Heal](component);
+                }
             }
         }
 
-        private static void DealDamage(List<GameObject> targets, int damage) {
+
+        private static IEnumerator DealDamage(List<GameObject> targets, int damage) {
             foreach (var target in targets) {
-                if(target.TryGetComponent(typeof(IDamageable), out var component))
+                if (target.TryGetComponent(typeof(IDamageable), out var component)) {
                     ((IDamageable)component).TakeDamage(damage);
+                    yield return Animation[EffectName.DealDamage](component);
+                }
             }
         }
 
-        private static void ChangeAttack(List<GameObject> targets, int value) {
+        private static IEnumerator ChangeAttack(List<GameObject> targets, int value) {
             throw new System.NotImplementedException();
         }
     }
