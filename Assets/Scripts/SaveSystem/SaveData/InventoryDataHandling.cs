194a2a9c87eb34b84ccef7a0edfc832c4f664baf
KingaMendyk <s25275@pjwstk.edu.pl>
2024-10-24 17:34:19 +0200

    Inventory Data Handling v1

--- Assets/Scripts/SaveSystem/SaveData/InventoryDataHandling.cs
+++ Assets/Scripts/SaveSystem/SaveData/InventoryDataHandling.cs
@@ -1,28 +1,72 @@
 using System.Collections.Generic;
 using Esper.ESave;
+using Items;
 using UnityEngine;
 
 namespace SaveSystem.SaveData {
     public class InventoryDataHandling:MonoBehaviour, ISavable {
-        private List<GameObject> allItems;
-        private List<GameObject> allCardSets;
-        private List<GameObject> allDeckCardSets;
+        private List<ItemSlot> allItems;
+        private List<ItemSlot> allCardSets;
+        private List<ItemSlot> allDeckCardSets;
 
-        private const string ItemSaveID = "Inventory item ";
-        private const string CardSetSaveID = "Inventory Card Set ";
-        private const string DeckSaveID = "Inventory deck ";
+        private const string ItemSaveID = "Inventory items";
+        private const string CardSetSaveID = "Inventory Card Set";
+        private const string DeckSaveID = "Inventory deck";
 
-        private void Awake() {
-            //TODO read all items and card sets
+        private void Start() {
+            allItems = InventoryController.Instance.GetItemSlots();
+            allCardSets = InventoryController.Instance.GetCardSetSlots();
+            allDeckCardSets = InventoryController.Instance.GetDeckSlots();
         }
         
         
         public void PopulateSaveData(SaveFile saveFile) {
-            
+            saveFile.AddOrUpdateData(ItemSaveID, ItemSaveID);
+            foreach (ItemSlot itemSlot in allItems) {
+                string itemSlotID = itemSlot.GetID();
+                
+                if (saveFile.HasData(itemSlotID))
+                    saveFile.DeleteData(itemSlotID);
+                saveFile.AddOrUpdateData(itemSlotID, itemSlot.IsOccupied());
+                
+                if (!itemSlot.IsOccupied()) {
+                    continue;
+                }
+                
+                CollectibleItem item = (CollectibleItem)itemSlot.GetItem();
+                string itemName = item.GetName();
+                Sprite itemSprite = item.GetSprite();
+                CollectibleItemData itemData = item.GetItemData();
+                string id = itemData.itemID;
+                
+                saveFile.AddOrUpdateData(itemSlotID + "_item", id);
+                saveFile.AddOrUpdateData(id + "_name", itemName);
+                //saveFile.AddOrUpdateData(id + "_sprite", itemSprite);
+            }
         }
 
         public void LoadSaveData(SaveFile saveFile) {
+            if(!saveFile.HasData(ItemSaveID))
+                return;
             
+            foreach (ItemSlot itemSlot in allItems) {
+                string itemSlotID = itemSlot.GetID();
+                itemSlot.SetIsOccupied(saveFile.GetData<bool>(itemSlotID));
+
+                if (itemSlot.IsOccupied()) {
+                    CollectibleItemData itemData = ScriptableObject.CreateInstance<CollectibleItemData>();
+                    string id = saveFile.GetData<string>(itemSlotID + "_item");
+                    itemData.itemID = id;
+                    GameObject itemObject = new GameObject();
+                    itemObject.AddComponent<DraggableItem>();
+                    CollectibleItem item = itemObject.AddComponent<CollectibleItem>();
+                    item.SetItemData(itemData);
+                    item.SetName(saveFile.GetData<string>(id + "_name"));
+                    //item.SetSprite(saveFile.GetData<Sprite>(id + "_sprite"));
+                    
+                    itemSlot.AddItem(item);
+                }
+            }
         }
     }
 }
